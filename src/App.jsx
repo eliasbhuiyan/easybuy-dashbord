1a54761581715b308feb5538f0bf1c5e9e2654e3
bhuiyan2001 <fathanbhuiyan2001@gmail.com>
2024-02-26 20:05:16 +0600

    calculator

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Error from "./components/Error";
 import Layout from "./components/Layout";
 import Dashbord from "./components/pages/Dashbord";
 import Calander from "./components/pages/Calander";
+import Calculator from "./components/pages/Calculator";
 
 function App() {
   const router = createBrowserRouter(
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashbord />} />
           <Route path="/calandar" element={<Calander />} />
+          <Route path="/calculator" element={<Calculator />} />
         </Route>
         <Route path="*" element={<Error />} />
       </Route>
