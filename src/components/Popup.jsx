23c89e7cd9e01cdfdb9186b872d0c344c485f8f1
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2024-02-28 17:33:46 +0600

    delete product

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,5 +1,5 @@
 import { AiFillDelete } from "react-icons/ai";
-const Popup = ({sendData}) => {
+const Popup = ({ sendData }) => {
   return (
     <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-[rgba(0,0,0,0.5)] z-[999]">
       <div className="w-[250px] flex flex-col p-4 items-center justify-center bg-gray-800 border border-gray-800 shadow-lg rounded-2xl">
@@ -15,10 +15,16 @@ const Popup = ({sendData}) => {
             </p>
           </div>
           <div className="p-2 mt-2 text-center space-x-1 md:block">
-            <button onClick={() => sendData(false)} className="mb-2 md:mb-0 bg-green-700 px-5 py-2 text-sm shadow-sm font-medium tracking-wider border-2 border-green-600 hover:border-green-700 text-gray-300 rounded-full hover:shadow-lg hover:bg-green-800 transition ease-in duration-300">
+            <button
+              onClick={() => sendData(false)}
+              className="mb-2 md:mb-0 bg-green-700 px-5 py-2 text-sm shadow-sm font-medium tracking-wider border-2 border-green-600 hover:border-green-700 text-gray-300 rounded-full hover:shadow-lg hover:bg-green-800 transition ease-in duration-300"
+            >
               Cancel
             </button>
-            <button onClick={() => sendData(true)} className="bg-red-500 hover:bg-red-600 px-5 ml-4 py-2 text-sm shadow-sm hover:shadow-lg font-medium tracking-wider border-2 border-red-300 hover:border-red-500 text-white rounded-full transition ease-in duration-300">
+            <button
+              onClick={() => sendData(true)}
+              className="bg-red-500 hover:bg-red-600 px-5 ml-4 py-2 text-sm shadow-sm hover:shadow-lg font-medium tracking-wider border-2 border-red-300 hover:border-red-500 text-white rounded-full transition ease-in duration-300"
+            >
               Delete
             </button>
           </div>
