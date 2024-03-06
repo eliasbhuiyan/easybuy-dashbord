f2f04041fc1f123cc2c8183b76829d59b207385e
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2024-03-06 12:02:19 +0600

    merchant approved

--- src/components/pages/Merchant.jsx
+++ src/components/pages/Merchant.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { GiCrossMark } from "react-icons/gi";
 import { IoCheckmarkDoneSharp } from "react-icons/io5";
+import { ToastContainer, toast } from "react-toastify";
 
 const Merchant = () => {
   let [merchant, setMerchant] = useState([]);
@@ -15,8 +16,29 @@ const Merchant = () => {
         console.log(err);
       });
   }, []);
+  const handelApproved = (id) => {
+    axios.post(`${import.meta.env.VITE_API_URL}auth/approvedmerchant`, {
+      id
+    }).then((res) => {
+      toast.success(res.data.message, {
+        position: "top-right",
+        autoClose: 5000,
+        closeOnClick: true,
+        theme: "light",
+      });
+    }).catch((err) => {
+      console.log(err);
+      toast.error(err.response.data.error, {
+        position: "top-right",
+        autoClose: 5000,
+        closeOnClick: true,
+        theme: "light",
+      });
+    })
+  }
   return (
     <section className="p-6 w-full productlist">
+      <ToastContainer/>
       <table className="w-full">
         <thead className="py-4 bg-secondary">
           <tr>
@@ -33,7 +55,7 @@ const Merchant = () => {
               <td>{item?.merchant?.email}</td>
               <td>{item?.merchant?.role}</td>
               <td className="flex items-center justify-evenly">
-                <button className="approved_btn">
+                <button onClick={()=>handelApproved(item._id)} className="approved_btn">
                   <IoCheckmarkDoneSharp className="edit_icon" />
                 </button>
                 <button className="reject_btn">
