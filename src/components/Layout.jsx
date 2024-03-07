3861195c644bd6f006e659a9efb4805f15f12f3d
bhuiyan2001 <fathanbhuiyan2001@gmail.com>
2024-03-08 04:48:56 +0600

    secured route 100%

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,22 +1,31 @@
 import { Outlet, Navigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import { useState } from "react";
-
 const Layout = () => {
   const [userRole, setUserRole] = useState("");
   const decodeToken = (token) => {
+    if (!token) return null;
     const payload = token.split(".")[1];
     const decodedPayload = atob(payload);
     return JSON.parse(decodedPayload);
   };
 
-  const token = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("sec_token="))
-    .split("=")[1];
+  const getCookie = () => {
+    if (document.cookie.length > 0) {
+      const token = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("sec_token="))
+        .split("=")[1];
+      return token;
+    }
+  };
 
-  const decodedToken = decodeToken(token);
-  return decodedToken.role == "admin" || decodedToken.role == "merchant" ? (
+  // const token = document.cookie
+  //   .split("; ")
+  //   .find((row) => row.startsWith("sec_token="))
+  //   .split("=")[1];
+  const decodedToken = decodeToken(getCookie());
+  return decodedToken?.role == "admin" || decodedToken?.role == "merchant" ? (
     <div className="flex">
       <Navbar />
       <Outlet />
