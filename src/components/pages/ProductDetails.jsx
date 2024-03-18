faf1be4db8b23abe89dc951f81a5c7cece04f7dc
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2024-03-18 14:57:29 +0600

    details

--- src/components/pages/ProductDetails.jsx
+++ src/components/pages/ProductDetails.jsx
@@ -1,6 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa";
 import { FaRegStar } from "react-icons/fa";
+import { useSelector } from "react-redux";
 const ProductDetails = () => {
+  const user = useSelector((state) => state.user_sec.user);
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    // Fetch Data
+    axios
+      .post(
+        `${import.meta.env.VITE_API_URL}product/findoneproduct`,
+        {
+          id: "#2929",
+        },
+        {
+          headers: {
+            Authorization: `Bearer user@${user?.auth}@${
+              import.meta.env.VITE_SWTSECRT
+            }`,
+          },
+        }
+      )
+      .then((res) => {
+        setProduct(res.data.product);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  console.log(product.variant);
   return (
     <div className="w-full p-6">
       <div className="border-b pb-4 mb-6 flex justify-around">
@@ -20,33 +53,31 @@ const ProductDetails = () => {
       <div className="flex gap-6">
         <div className="w-1/2">
           <div className="flex gap-1">
-            <div className="border">
-              <img src="/Image.png" alt="img" />
-            </div>
-            <div className="border">
-              <img src="/Image.png" alt="img" />
-            </div>
-            <div className="border">
-              <img src="/Image.png" alt="img" />
-            </div>
+            {product?.variant?.map((item) => (
+              <div key={item?._id} className="border">
+                <img src={item?.image} alt="img" />
+              </div>
+            ))}
           </div>
           <div className="w-56 flex gap-1 m-auto mt-2">
-            <div className="border">
-              <img src="/Image.png" alt="img" />
-            </div>
-            <div className="border">
-              <img src="/Image.png" alt="img" />
-            </div>
-            <div className="border">
-              <img src="/Image.png" alt="img" />
-            </div>
+            {product?.variant?.map((item) => (
+              <div key={item?._id} className="border">
+                <img src={item?.image} alt="img" />
+              </div>
+            ))}
           </div>
         </div>
         <div className="w-1/2">
           <div>
-            <h3 className="title text-start capitalize">
-              iphone 15 Pro Max 256GB/512GB/1TB
+            <h3 className="title mb-2 text-start capitalize">
+              {product?.name}
             </h3>
+            <h4 className="title mb-2 text-start capitalize">
+              ID: {product?.shortID}
+            </h4>
+            <button className="bg-green-500 text-white py-1 px-4 rounded-xl">
+              {product?.status}
+            </button>
             <div className="flex items-center gap-2 my-2">
               <ul className="flex gap-1 text-orange-400">
                 <li>
@@ -68,22 +99,27 @@ const ProductDetails = () => {
               <p>8 Reviews</p>
             </div>
             <div className="border-b">
-              <h3 className="title text-start text-brand">$44.00</h3>
+              {product?.variant?.map((item) => (
+                <h3
+                  key={item?._id}
+                  className="title mb-0 text-start text-brand"
+                >
+                  {item?.price}Tk
+                </h3>
+              ))}
             </div>
             <div className="flex gap-2 items-center mt-4">
               <p className="text-xl font-sans uppercase font-semibold text-primary  text-start">
                 Color:
               </p>
               <ul className="flex gap-1 items-center mt-2">
-                <li>
-                  <span className="w-4 h-4 rounded-full bg-[red] inline-block"></span>
-                </li>
-                <li>
-                  <span className="w-4 h-4 rounded-full bg-[green] inline-block"></span>
-                </li>
-                <li>
-                  <span className="w-4 h-4 rounded-full bg-[black] inline-block"></span>
-                </li>
+                {product?.variant?.map((item) => (
+                  <li key={item?._id}>
+                    <span
+                      className={`w-6 h-6 cursor-pointer rounded-full border bg-[${item?.color}] inline-block`}
+                    ></span>
+                  </li>
+                ))}
               </ul>
             </div>
             <div className="flex gap-2 items-center my-4">
@@ -91,47 +127,47 @@ const ProductDetails = () => {
                 Size:
               </p>
               <ul className="flex gap-1 items-center">
-                <li>
-                  <span className="py-1 px-2 rounded-sm border inline-block">
-                    XL
-                  </span>
-                </li>
-                <li>
-                  <span className="py-1 px-2 rounded-sm border border-brand inline-block">
-                    LG
-                  </span>
-                </li>
-                <li>
-                  <span className="py-1 px-2 rounded-sm border inline-block">
-                    MD
-                  </span>
-                </li>
+                {product?.variant?.map((item) => (
+                  <li key={item?._id}>
+                    <span className="py-1 px-2 rounded-sm border inline-block">
+                      {item?.size}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="flex gap-2 items-center my-4">
+              <p className="text-xl font-sans uppercase font-semibold text-primary  text-start">
+                Storage:
+              </p>
+              <ul className="flex gap-1 items-center">
+                {product?.variant?.map((item) => (
+                  <li key={item?._id}>
+                    <span className="py-1 px-2 rounded-sm border inline-block">
+                      {item?.storage}
+                    </span>
+                  </li>
+                ))}
               </ul>
             </div>
             <div className="flex gap-2 items-center">
               <p className="text-xl font-sans uppercase font-semibold text-primary  text-start">
                 Quantity:
+                {product?.variant?.map((item) => (
+                  <li key={item?._id}>
+                    <span className="py-1 px-2 rounded-sm border inline-block">
+                      {item?.quantity}
+                    </span>
+                  </li>
+                ))}
               </p>
-              <div className="flex justify-start w-28 border">
-                <button className="w-1/3 font-bold py-2 border-r">-</button>
-                <input
-                  className="w-1/3 font-bold py-2 outline-none text-center"
-                  value={1}
-                  readOnly
-                />
-                <button className="w-1/3 font-bold py-2 border-l">+</button>
-              </div>
             </div>
           </div>
         </div>
       </div>
       <div className="my-8">
         <p className="title text-start mb-2">Product Details:</p>
-        <p className="basic">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Reprehenderit, tempore! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Placeat, ducimus.
-        </p>
+        <p className="basic">{product?.description}</p>
       </div>
       <div className="my-4">
         <p className="title text-start mb-2 mt-8">Customer Review (5) :</p>
